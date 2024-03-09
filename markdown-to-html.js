ccf31296844e73bd49b702fb284a6b3a2dbed715
Yevhenii Kovalov <kovaliov.evgheniy@gmail.com>
2024-03-09 21:16:25 +0200

    Revert "Import main components"
    This reverts commit 79d699f93b54c87fd1479073f864d4bc788adc4f.

--- markdown-to-html.js
+++ markdown-to-html.js
@@ -1,16 +1,1 @@
 'use strict';
-
-const fs = require('node:fs');
-const program = require('commander');
-
-const Converter = require('./converter.js');
-
-const MARKUP = {
-  '**': 'b',
-  '_': 'i',
-  '`': 'tt',
-  '```': 'pre',
-  '\n': 'p',
-};
-
-const converter = new Converter(MARKUP);
