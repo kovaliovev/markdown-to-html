d7c8159fae87e55dc36a670543b0a47cfc8da87f
Yevhenii Kovalov <kovaliov.evgheniy@gmail.com>
2024-03-09 20:06:52 +0200

    Add main converting method

--- converter.js
+++ converter.js
@@ -4,7 +4,7 @@ class Converter {
   constructor() {
     this.markup = {
       '**': 'b',
-      '_': 'i',
+      _: 'i',
       '`': 'tt',
       '```': 'pre',
       '\n': 'p',
@@ -15,6 +15,21 @@ class Converter {
     console.error(`\x1b[31m${error}\x1b[0m`);
     process.exit(1);
   }
+
+  toHTML(mdText) {
+    const lines = mdText.split('\r\n');
+    const result = [];
+
+    for (let i = 0; i <= lines.length; i++) {
+      const line = lines[i];
+      const resultLine = this.processLine(line);
+
+      if (typeof resultLine === 'string') result.push(resultLine);
+    }
+    return result.join('\n').replace(/\n<\/p>/g, '</p>');
+  }
+
+  processLine(line) {}
 }
 
 module.exports = { Converter };
