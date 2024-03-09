c935807ef00c1b36f3f7705ba12bf2b276cb6c78
Yevhenii Kovalov <kovaliov.evgheniy@gmail.com>
2024-03-09 21:49:04 +0200

    Add CLI part of application

--- markdown-to-html.js
+++ markdown-to-html.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const { program } = require('commander');
-
 const { Converter } = require('./converter.js');
 
 const MARKUP = {
@@ -14,3 +13,28 @@ const MARKUP = {
 };
 
 const converter = new Converter(MARKUP);
+
+program
+  .version('1.0.0')
+  .argument('<input>', 'Path to the input Markdown file')
+  .option('-o, --output <output>', 'Path to the output HTML file')
+  .parse(process.argv);
+
+(async () => {
+  const inputPath = program.args[0];
+  const outputPath = program.opts().output;
+
+  const data = await fs.readFile(inputPath, 'utf8').catch((err) => {
+    console.error('\x1b[31mError during reading input file:\x1b[0m', err);
+    process.exit(1);
+  });
+
+  const html = converter.toHTML(data);
+
+  if (outputPath) {
+    await fs.writeFile(outputPath, html).catch((err) => {
+      console.error('\x1b[31mError during writing to output file:\x1b[0m', err);
+      process.exit(1);
+    });
+  } else console.log(html);
+})();
