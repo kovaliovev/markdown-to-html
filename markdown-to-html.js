d0eb1770b4770bcb233b71d937cc07d0a97acf33
Yevhenii Kovalov <kovaliov.evgheniy@gmail.com>
2024-03-09 21:17:30 +0200

    Import main components properly

--- markdown-to-html.js
+++ markdown-to-html.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const fs = require('node:fs');
+const { program } = require('commander');
+
+const Converter = require('./converter.js');
+
+const MARKUP = {
+  '**': 'b',
+  '_': 'i',
+  '`': 'tt',
+  '```': 'pre',
+  '\n': 'p',
+};
+
+const converter = new Converter(MARKUP);
