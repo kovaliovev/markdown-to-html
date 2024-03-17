e9dc04b3a544effa337df8b0702379897d9fa537
Evgheniy Kovaliov <kovaliov.evgeniy11@gmail.com>
2024-03-17 22:56:20 +0200

    Add test cases for ANSI format

--- test/converter.test.js
+++ test/converter.test.js
@@ -4,6 +4,7 @@ const { Converter } = require('../lib/converter.js');
 const { MARKUP } = require('../lib/markup.js');
 
 const cHTML = new Converter(MARKUP['html']);
+const cANSI = new Converter(MARKUP['ansi']);
 
 describe('Valid md to html converting', () => {
   test('bold', () => {
@@ -43,28 +44,28 @@ describe('Valid md to html converting', () => {
 describe('Invalid md to html converting', () => {
   test('unclosed bold', () => {
     expect(() =>
-      c
+      cHTML
         .fromMarkdown('This is a **bold word.')
         .toThrow('Found unclosed md flag!')
     );
   });
   test('unclosed italic', () => {
     expect(() =>
-      c
+      cHTML
         .fromMarkdown('This is a italic_ word.')
         .toThrow('Found unclosed md flag!')
     );
   });
   test('unclosed monospaced', () => {
     expect(() =>
-      c
+      cHTML
         .fromMarkdown('This is a `monospaced word.')
         .toThrow('Found unclosed md flag!')
     );
   });
   test('unclosed preformatted', () => {
     expect(() =>
-      c
+      cHTML
         .fromMarkdown(
           '```\r\nThis is **preformatted** _text_\r\n\r\nThis is _preformatted_ `text too`\r\n'
         )
@@ -73,7 +74,7 @@ describe('Invalid md to html converting', () => {
   });
   test('nested-1', () => {
     expect(() =>
-      c
+      cHTML
         .fromMarkdown('This is a **_italico-bold_** word.')
         .toThrow('Found nested md flag!')
     );
@@ -87,7 +88,98 @@ describe('Invalid md to html converting', () => {
   });
   test('nested-3', () => {
     expect(() =>
-      c
+      cHTML
+        .fromMarkdown('This is a ****very bold**** word.')
+        .toThrow('Found nested md flag!')
+    );
+  });
+});
+
+describe('Valid md to ansi converting', () => {
+  test('bold', () => {
+    expect(cANSI.fromMarkdown('This is a **bold** word')).toBe(
+      'This is a \x1b[1mbold\x1b[0m word\n'
+    );
+  });
+  test('italic', () => {
+    expect(cANSI.fromMarkdown('This is a _italic_ word.')).toBe(
+      'This is a \x1b[3mitalic\x1b[0m word.\n'
+    );
+  });
+  test('monospaced', () => {
+    expect(cANSI.fromMarkdown('This is a `monospaced` word.')).toBe(
+      'This is a \x1b[7mmonospaced\x1b[m word.\n'
+    );
+  });
+  test('preformatted', () => {
+    expect(
+      cANSI.fromMarkdown(
+        '```\r\nThis is **preformatted** _text_\r\n\r\nThis is _preformatted_ `text too`\r\n```'
+      )
+    ).toBe(
+      '\x1b[7m\nThis is **preformatted** _text_\n\nThis is _preformatted_ `text too`\n\x1b[m\n'
+    );
+  });
+  test('empty', () => {
+    expect(cANSI.fromMarkdown(' ')).toBe(' \n');
+  });
+  test('multi paragraphs', () => {
+    expect(
+      cANSI.fromMarkdown('**First** _paragraph_\r\n\r\n`Second` line')
+    ).toBe(
+      '\x1b[1mFirst\x1b[0m \x1b[3mparagraph\x1b[0m\n\n\x1b[7mSecond\x1b[m line\n'
+    );
+  });
+});
+
+describe('Invalid md to ansi converting', () => {
+  test('unclosed bold', () => {
+    expect(() =>
+      cANSI
+        .fromMarkdown('This is a **bold word.')
+        .toThrow('Found unclosed md flag!')
+    );
+  });
+  test('unclosed italic', () => {
+    expect(() =>
+      cANSI
+        .fromMarkdown('This is a italic_ word.')
+        .toThrow('Found unclosed md flag!')
+    );
+  });
+  test('unclosed monospaced', () => {
+    expect(() =>
+      cANSI
+        .fromMarkdown('This is a `monospaced word.')
+        .toThrow('Found unclosed md flag!')
+    );
+  });
+  test('unclosed preformatted', () => {
+    expect(() =>
+      cANSI
+        .fromMarkdown(
+          '```\r\nThis is **preformatted** _text_\r\n\r\nThis is _preformatted_ `text too`\r\n'
+        )
+        .toThrow('Found unclosed md flag!')
+    );
+  });
+  test('nested-1', () => {
+    expect(() =>
+      cANSI
+        .fromMarkdown('This is a **_italico-bold_** word.')
+        .toThrow('Found nested md flag!')
+    );
+  });
+  test('nested-2', () => {
+    expect(() =>
+      cANSI
+        .fromMarkdown('**`_this is invalid_`**')
+        .toThrow('Found nested md flag!')
+    );
+  });
+  test('nested-3', () => {
+    expect(() =>
+      cANSI
         .fromMarkdown('This is a ****very bold**** word.')
         .toThrow('Found nested md flag!')
     );
